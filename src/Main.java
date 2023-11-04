9dbb5d66bd702a1523fbd60907f59a71ba35447c
ADMIN <ADMIN@LAPTOP-P1NOGJ1A>
2023-11-04 11:17:31 +0700

    CSV

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+import controller.Algorithm;
+import java.util.ArrayList;
+import model.CSV;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        Algorithm a= new Algorithm ();
+        ArrayList<CSV> ls = new ArrayList<>();
+        ls.add(new CSV(1, "Le Van Nguyen", "nguyen@gmail.com", "'0982320356", "Thuy Phuong   - hUong Thuy -  Hue"));
+        ls.add(new CSV(2, "NGUyen tHi         nGoc   ", "ngoc@gmail.com", "'0986431545", "Tran Hung dAo - DiEn nGoc   -   Quang nAm"));
+        ls.add(new CSV(3, "Nguyen   van MiNh      	", "minh@gmail.com", "'0976256314", "Cau Giay     - Ha    Noi    - Viet Nam    "));
+        a.display(ls);
+    }
+}
